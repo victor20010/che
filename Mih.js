08a015036f1b7e5488c60993553b298b573f1a29
victor20010 <99201444+victor20010@users.noreply.github.com>
2023-04-24 13:21:56 +0200

    Update Mih.js

--- Mih.js
+++ Mih.js
@@ -1,119 +1,63 @@
-example of how to use the modified function with or without sticker calculations, as well as comments to explain each step:
-
-```javascript
-// Calculate price without sticker
-calculateItemPrice("AK-47 | Redline (Field-Tested)", 730, 5, 5, 10, true, 30, false, false, 10, 0, false);
-
-// Calculate price with sticker
-calculateItemPrice("AK-47 | Redline (Field-Tested)", 730, 5, 5, 10, true, 30, false, false, 10, 0, true);
-```
-
-In the first function call, the last argument for `calculateItemPrice()` is `false`, which means sticker calculations will be skipped.
-
-In the second function call, the last argument is `true`, which means sticker calculations will be included.
-
-Here's an explanation of each argument used in the function:
-
-- `itemName` (string): the name of the item to calculate the price for.
-- `appId` (number): the Steam App ID for the item.
-- `corridor` (number): the percentage range of prices to consider. For example, a corridor of 5 means only prices within 5% of the median will be considered.
-- `profitPercentage` (number): the desired profit percentage when selling the item.
-- `pleasantProfit` (number): the minimum profit percentage that is acceptable before the buy price is increased.
-- `profitUpdate` (boolean): whether or not to recalculate the buy and sell prices after an actual sale.
-- `analysisPeriod` (number): the number of days to consider when analyzing price history.
-- `analyzeBeforeOrder` (boolean): whether or not to adjust the buy price based on the last purchase order price.
-- `analyzeBeforeSale` (boolean): whether or not to adjust the sell price based on the first listing price in the user's inventory.
-- `stickerMarkupPercentage` (number): the percentage to markup the average sticker price by.
-- `minimumStickerMarkup` (number): the minimum amount to markup the sticker price by, regardless of the `stickerMarkupPercentage`.
-- `includeStickers` (boolean): whether or not to consider sticker prices when calculating buy and sell prices.
-
-Make sure to replace "AK-47 | Redline (Field-Tested)" with the actual name of the item you want to calculate the price for, and set the correct `appId`.
-
-
-// Calculate price without sticker
-calculateItemPrice("AK-47 | Redline (Field-Tested)", 730, 5, 5, 10, true, 30, false, false, 10, 0, false);
-
-// Calculate price with sticker
-calculateItemPrice("AK-47 | Redline (Field-Tested)", 730, 5, 5, 10, true, 30, false, false, 10, 0, true);
-
-/**
- * Calculates the recommended buy and sell prices for a Steam item based on recent price history and user-defined settings.
- * @param {string} itemName - Name of the item to analyze.
- * @param {number} appId - ID of the Steam application that the item belongs to.
- * @param {number} corridor - Percentage deviation from the median price to include in the analysis.
- * @param {number} profitPercentage - Desired percentage profit from selling the item.
- * @param {number} pleasantProfit - Minimum percentage profit that is considered acceptable.
- * @param {number} profitUpdate - Number of days between updates to the price analysis. Minimum is 1.
- * @param {number} analysisPeriod - Number of days to include in the price analysis. Minimum is 1.
- * @param {boolean} analyzeBeforeOrder - Whether to analyze prices before placing a buy order.
- * @param {boolean} analyzeBeforeSale - Whether to analyze prices before listing the item for sale.
- * @param {number} stickerMarkupPercentage - Percentage markup to apply to the average sticker price.
- * @param {number} minimumStickerMarkup - Minimum markup to apply to the average sticker price.
- * @param {boolean} includeStickers - Whether to include sticker listings in the price analysis.
- */
-
-
-
 function calculateItemPrice(itemName, appId, corridor, profitPercentage, pleasantProfit, profitUpdate, analysisPeriod, analyzeBeforeOrder, analyzeBeforeSale, stickerMarkupPercentage, minimumStickerMarkup, includeStickers) {
-  // Calculate start and end dates for price analysis
+  // Calculate start and end dates for price analysis (Рассчитываем даты начала и окончания анализа цен)
   const endTimestamp = Math.floor(Date.now() / 1000);
   const startTimestamp = endTimestamp - analysisPeriod * 24 * 60 * 60;
 
-  // Fetch price history
+  // Fetch price history (Получаем историю цен)
   const url = `https://steamcommunity.com/market/pricehistory/?appid=${appId}&market_hash_name=${encodeURIComponent(itemName)}&start=${startTimestamp}&end=${endTimestamp}`;
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      // filter prices using price corridor
+      // filter prices using price corridor (Фильтруем цены по ценовому коридору)
       const prices = data.prices.filter(price => price[1] >= corridor && price[1] <= (100-corridor));
 
-      // calculate sticker markup
+      // calculate sticker markup (Рассчитываем наценку за стикеры)
       let stickerMarkup = 0;
-      if (includeStickers) { // check if includeStickers is true
-        const stickerPricesOnly = prices.filter(price => price[2]); // filter out listings without stickers
+      if (includeStickers) { // check if includeStickers is true (Проверяем, включены ли стикеры)
+        const stickerPricesOnly = prices.filter(price => price[2]); // filter out listings without stickers (Фильтруем записи без стикеров)
         if (stickerPricesOnly.length > 0) {
-          const pastWeekStickerPrices = stickerPricesOnly.slice(0, 7); // last 7 days of sticker prices
+          const pastWeekStickerPrices = stickerPricesOnly.slice(0, 7); // last 7 days of sticker prices (Последние 7 дней цен на стикеры)
           const averageStickerPrice = pastWeekStickerPrices.reduce((total, price) => total + price[2], 0) / pastWeekStickerPrices.length;
 
-          // calculate sticker markup
+          // calculate sticker markup (Рассчитываем наценку за стикеры)
           stickerMarkup = averageStickerPrice * (stickerMarkupPercentage / 100);
 
-          // enforce minimum sticker markup
+          // enforce minimum sticker markup (Принудительно назначаем минимальную наценку за стикеры)
           if (stickerMarkup < minimumStickerMarkup) {
             stickerMarkup = minimumStickerMarkup;
           }
         } else {
-          console.log("No listings with stickers found.");
+          console.log("No listings with stickers found."); // (Нет стикеров в продаже)
         }
       }
 
-      // calculate buy and sell prices
+      // calculate buy and sell prices (Рассчитываем цены покупки и продажи)
       let buyPrice = prices[0][1];
-      const stickerAdjustedBuyPrice = Math.max(1, buyPrice + stickerMarkup); // minimum is 1 due to Steam's minimum sale price rule
+      const stickerAdjustedBuyPrice = Math.max(1, buyPrice + stickerMarkup); // minimum is 1 due to Steam's minimum sale price rule (Минимальное значение 1, из-за правил Steam)
       let sellPrice = Math.floor(stickerAdjustedBuyPrice * (1 + profitPercentage / 100) * (1 - 0.15));
-      const steamFee = Math.floor(sellPrice * 0.15); // calculate fee here
+      const steamFee = Math.floor(sellPrice * 0.15); // calculate fee here (Вычисляем комиссию здесь)
       let actualProfit = Math.floor((sellPrice - stickerAdjustedBuyPrice) / stickerAdjustedBuyPrice * 100);
 
-      // recalculate prices if pleasant profit not met
+      // recalculate prices if pleasant profit not met (Пересчитываем цены, если прибыль меньше требуемой)
       while (actualProfit < pleasantProfit) {
-        buyPrice += 1; // increase buy price by 1 unit
+        buyPrice += 1; // increase buy price by 1 unit (Увеличиваем цену покупки на 1 единицу)
         const newStickerAdjustedBuyPrice = Math.max(1, buyPrice + stickerMarkup);
         const newSellPrice = Math.floor(newStickerAdjustedBuyPrice * (1 + profitPercentage / 100) * (1 - 0.15));
         const newSteamFee = Math.floor(newSellPrice * 0.15);
         const newActualProfit = Math.floor((newSellPrice - newStickerAdjustedBuyPrice) / newStickerAdjustedBuyPrice * 100);
         if (newActualProfit > actualProfit) {
           sellPrice = newSellPrice;
-          steamFee = newSteamFee; // update fee here
+          steamFee = newSteamFee; // update fee here (Обновляем комиссию здесь)
           actualProfit = newActualProfit;
         } else {
-          break; // stop increasing buy price if profit stops increasing
+          break; // stop increasing buy price if profit stops increasing (Прекращаем увеличение цены покупки, если прибыль перестанет увеличиваться)
         }
       }
 
-      // adjust sell price for Steam fee
+      // adjust sell price for Steam fee (Корректируем цену продажи, учитывая комиссию Steam)
       sellPrice -= steamFee;
 
-      // check purchase requests
+      // check purchase requests (Проверяем заказы на покупку)
       const nameid = data.nameid;
       const requestsUrl = `https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid=${nameid}&two_factor=0`;
       fetch(requestsUrl)
@@ -122,17 +66,17 @@ function calculateItemPrice(itemName, appId, corridor, profitPercentage, pleasan
           const buyRequests = requests.buy_order_graph[0].quantity;
           const sellRequests = requests.sell_order_graph[0].quantity;
 
-          // adjust buy/sell prices based on purchase requests
+          // adjust buy/sell prices based on purchase requests (Корректируем цены покупки/продажи на основе заказов на покупку)
           if (buyRequests > sellRequests) {
-            // Increase buy price to stand a better chance of getting the item
+            // Increase buy price to stand a better chance of getting the item (Увеличиваем цену покупки, чтобы увеличить вероятность получения предмета)
             buyPrice = Math.ceil(buyPrice * (buyRequests / sellRequests));
           } else if (sellRequests > buyRequests) {
-            // Decrease sell price to sell the item more quickly
+            // Decrease sell price to sell the item more quickly (Уменьшаем цену продажи, чтобы продать предмет быстрее)
             sellPrice = Math.floor(sellPrice * (sellRequests / buyRequests));
           }
 
-          // analyze before order
-          if (analyzeBeforeOrder) { // check if analyzeBeforeOrder is true
+          // analyze before order (Анализируем перед заказом)
+          if (analyzeBeforeOrder) { // check if analyzeBeforeOrder is true (Проверяем, включен ли анализ перед заказом)
             const orderUrl = `https://steamcommunity.com/market/itemordershistogram?country=US&language=english&currency=1&item_nameid=${nameid}&two_factor=0&norender=1`;
             fetch(orderUrl)
               .then(response => response.json())
@@ -141,13 +85,13 @@ function calculateItemPrice(itemName, appId, corridor, profitPercentage, pleasan
                 const newBuyPrice = Math.floor(lastOrderPrice * 100 + 1) / 100;
                 if (newBuyPrice < buyPrice) {
                   buyPrice = newBuyPrice;
-                  console.log(`Adjusted buy price before order: ${buyPrice}`);
+                  console.log(`Adjusted buy price before order: ${buyPrice}`); // (Корректировка цены покупки перед заказом)
                 }
               });
           }
 
-          // analyze before sale
-          if (analyzeBeforeSale) { // check if analyzeBeforeSale is true
+          // analyze before sale (Анализируем перед продажей)
+          if (analyzeBeforeSale) { // check if analyzeBeforeSale is true (Проверяем, включен ли анализ перед продажей)
             const inventoryUrl = `https://steamcommunity.com/inventory/YOUR_STEAM_ID/YOUR_APP_ID/YOUR_CONTEXT_ID?count=5000`;
             fetch(inventoryUrl)
               .then(response => response.json())
@@ -156,12 +100,48 @@ function calculateItemPrice(itemName, appId, corridor, profitPercentage, pleasan
                 const newSellPrice = Math.ceil(firstListingPrice * 100 - 1) / 100;
                 if (newSellPrice > sellPrice) {
                   sellPrice = newSellPrice;
-                  console.log(`Adjusted sell price before sale: ${sellPrice}`);
+                  console.log(`Adjusted sell price before sale: ${sellPrice}`); // (Корректировка цены продажи перед продажей)
                 }
               });
           }
 
-          console.log(`Buy at ${buyPrice}, sell at ${sellPrice} (profit: ${actualProfit}%, buy requests: ${buyRequests}, sell requests: ${sellRequests}), Steam fee: ${steamFee}`); // output fee here
+          // calculate sticker markup when selling the item
+          let stickerMarkupWhenSelling = 0;
+          if (includeStickers) { // check if includeStickers is true (Проверяем, включены ли стикеры)
+            const lastPrice = prices[0][1];
+            const lastStickerAdjustedPrice = Math.max(1, lastPrice + stickerMarkup);
+            const lastSellPrice = Math.floor(lastStickerAdjustedPrice * (1 + profitPercentage / 100) * (1 - 0.15));
+            const lastSteamFee = Math.floor(lastSellPrice * 0.15);
+            const lastActualProfit = Math.floor((lastSellPrice - lastStickerAdjustedPrice) / lastStickerAdjustedPrice * 100);
+            const stickerAdjustedSellPrice = Math.floor(stickerAdjustedBuyPrice * (1 + profitPercentage / 100) * (1 - 0.15));
+            const stickerAdjustedActualProfit = Math.floor((stickerAdjustedSellPrice - stickerAdjustedBuyPrice) / stickerAdjustedBuyPrice * 100);
+
+            // calculate sticker markup
+            stickerMarkupWhenSelling = (stickerAdjustedActualProfit - lastActualProfit) * stickerAdjustedBuyPrice / 100;
+          }
+
+          // return final buy and sell prices (Возвращаем итоговые цены покупки и продажи)
+          const result = {
+            buyPrice: buyPrice,
+            sellPrice: sellPrice,
+            stickerMarkupWhenSelling: stickerMarkupWhenSelling // наценка за стикеры при продаже предмета
+          };
+          return result;
         });
-    });
+
+      // update profit value every profitUpdate seconds (Обновляем прибыль каждые profitUpdate секунд)
+      setInterval(() => {
+        const newProfit = Math.floor((sellPrice - stickerAdjustedBuyPrice) / stickerAdjustedBuyPrice * 100);
+        if (newProfit > actualProfit) {
+          sellPrice += 1; // increase sell price by 1 unit (Увеличиваем цену продажи на 1 единицу)
+          steamFee = Math.floor(sellPrice * 0.15); // update fee here (Обновляем комиссию здесь)
+          sellPrice -= steamFee; // adjust sell price for Steam fee (Корректируем цену продажи, учитывая комиссию Steam)
+          actualProfit = newProfit;
+        }
+      }, profitUpdate * 1000); // с интервалом profitUpdate секунд
+    })
+    .catch(error => console.error(error));
 }
+
+// EXAMPLE USAGE: (Пример использования)
+calculateItemPrice("AK-47 | Redline (Field-Tested)", 730, 5, 10, 20, 60, 30, true, true, 5, 0.03, true);
